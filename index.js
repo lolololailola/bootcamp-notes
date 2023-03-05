b46b7956ad088282dca9fbaff339f0b331c34837
lolololailola <manuel.ruiz.roble@gmail.com>
2023-03-05 20:59:20 +0100

    added mongodb

--- index.js
+++ index.js
@@ -1,109 +1,86 @@
 import express from 'express'
-import { generateId } from './utils/utils.js'
 import cors from 'cors'
+
+import { Note } from './models/note.js'
+import { errorHandler } from './middleware/errorHandler.js'
+import { unknownEndpoint } from './middleware/unknownEndpoint.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
-
-// Servidor Sin Express, con el módulo nativo http
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json' })
-//   response.end(JSON.stringify(notes))
-// })
-
 app.get('/', (request, response) => {
   response.send('<h1>Hola Mundo desde express!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find((note) => note.id === id)
-
-  note ? response.json(note) : response.status(404).end()
-})
-
-app.get('/*', (request, response) => {
-  response.status(404).end()
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => {
+      next(error)
+    })
 })
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing',
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(notes),
+  })
+
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
 })
 
-app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find((n) => n.id === id)
-
-  if (note) {
-    const body = request.body
-
-    if (!body.content) {
-      return response.status(400).json({
-        error: 'content missing',
-      })
-    }
-
-    const modifiedNote = { ...note, important: body.important }
-
-    notes = notes.map((n) => (n.id !== id ? n : modifiedNote))
-    response.json(modifiedNote)
-  } else {
-    response.status(404).end()
-  }
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter((note) => note.id !== id)
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-  response.status(204).end()
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
 })
--- utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const generateId = (array) => {
-  const maxId = array.length > 0 ? Math.max(...array.map((a) => a.id)) : 0
-
-  return maxId + 1
-}
